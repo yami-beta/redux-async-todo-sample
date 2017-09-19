6fce024848cb1b23c5615867a26c51c3f2545491
yami_beta <yami-beta@users.noreply.github.com>
2017-09-19 15:21:33 +0900

    :+1: Add delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,10 +10,19 @@ const Todo = (props) => {
     });
   };
 
+  const onClick = (event) => {
+    event.preventDefault();
+    const { id } = props;
+    props.action.delete(id);
+  };
+
   return (
     <li>
       <input id={`todo-${props.id}`} type="checkbox" checked={props.complete} onChange={onChange} />
-      <label htmlFor={`todo-${props.id}`}>{props.text}</label>
+      <label htmlFor={`todo-${props.id}`}>
+        {props.text}
+        <button className="todo__delete" onClick={onClick}>x</button>
+      </label>
     </li>
   );
 };
